339ac48b3f05f6e3a284a8e0fc5c2f6eb606cfec
OwenDunkel <tt8664pj@go.minnstate.edu>
2025-03-19 12:30:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+public class TrieNode
+{
+    public Dictionary<char, TrieNode> Children { get; set; }
+
+    public bool IsEndOfWord { get; set; }
+
+    public TrieNode()
+    {
+        Children = new Dictionary<char, TrieNode>();
+        IsEndOfWord = false;
+    }
+}
+
+public class Trie
+{
+    private readonly TrieNode root; 
+
+    public Trie()
+    {
+        root = new TrieNode();
+    }
+
+    public void Insert(string word)
+    {
+        TrieNode current = root;
+        foreach (char ch in word)
+        {
+            if (!current.Children.ContainsKey(ch))
+            {
+                current.Children[ch] = new TrieNode(); 
+            }
+            current = current.Children[ch]; 
+        }
+        current.IsEndOfWord = true; 
+    }
+
+    private TrieNode SearchPrefix(string prefix)
+    {
+        TrieNode current = root;
+        foreach (char ch in prefix)
+        {
+            if (!current.Children.ContainsKey(ch))
+                return null;
+
+            current = current.Children[ch];
+        }
+        return current;
+    }
+}
